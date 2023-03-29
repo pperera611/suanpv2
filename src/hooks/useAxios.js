aaee884fc62ba5e63ee08b938af1f0ae954dff32
pperera611 <pperera611@gmail.com>
2023-03-29 16:57:36 -0300

    arreglada autenticacion con cambios en las reglas en firebase

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,44 +1,52 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { auth } from "../auth/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
 
-const useAxios = (method, apiRoute, options = {},reloadFlag = false) => {
+const useAxios = (method, apiRoute, options = {}, reloadFlag = false) => {
   const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [user, ] = useAuthState(auth);
+
+  const getUrlWithAuth = async (url) => {
+    const token = await user.getIdToken();
+    return `${url}?auth=${token}`;
+  };
 
   const fetchData = async () => {
-   
-      try {
-        setLoading(true);
-        if (method === "GET") {
-          const result = await axios({
-            method,
-            url: `https://suanp-f6399-default-rtdb.firebaseio.com/${apiRoute}.json`,
-            ...options,
-          });
-          
-          setResponse(result.data);
-          setError(null);
-        }
-      } catch (error) {
-        setError(error);
-      } finally {
-        setLoading(false);
-      }
-    //} 
-  };
-  const postData = async (postOptions = {}) => {
-   
     try {
       setLoading(true);
+      if (method === "GET") {
+        const url = await getUrlWithAuth(`https://suanp-f6399-default-rtdb.firebaseio.com/${apiRoute}.json`);
+        const result = await axios({
+          method,
+          url,
+          ...options,
+        });
+
+        setResponse(result.data);
+        setError(null);
+      }
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const postData = async (postOptions = {}) => {
+    try {
+      setLoading(true);
+      const url = await getUrlWithAuth(`https://suanp-f6399-default-rtdb.firebaseio.com/${apiRoute}.json`);
       const result = await axios({
         method: "post",
-        url: `https://suanp-f6399-default-rtdb.firebaseio.com/${apiRoute}.json`,
+        url,
         ...options,
         ...postOptions,
       });
       setResponse(result.data);
-     
+
       setError(null);
     } catch (error) {
       setError(error);
@@ -50,9 +58,10 @@ const useAxios = (method, apiRoute, options = {},reloadFlag = false) => {
   const putData = async (id, putOptions = {}) => {
     try {
       setLoading(true);
+      const url = await getUrlWithAuth(`https://suanp-f6399-default-rtdb.firebaseio.com/${apiRoute}/${id}.json`);
       const result = await axios({
         method: "put",
-        url: `https://suanp-f6399-default-rtdb.firebaseio.com/${apiRoute}/${id}.json`,
+        url,
         ...options,
         ...putOptions,
       });
@@ -65,14 +74,14 @@ const useAxios = (method, apiRoute, options = {},reloadFlag = false) => {
     }
   };
 
-
   useEffect(() => {
-    fetchData();
-    
+    if (user) {
+      fetchData();
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [method, apiRoute, JSON.stringify(options),reloadFlag]);
+  }, [method, apiRoute, JSON.stringify(options), reloadFlag, user]);
 
-  return {response, error, loading, postData, putData };
+  return { response, error, loading, postData, putData };
 };
 
 export default useAxios;
